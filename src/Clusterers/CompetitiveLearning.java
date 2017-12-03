f81efd6d97780159e06ba4b9d06dcea73aa67d5e
Pookey <pookey@Pookeys-MacBook-Pro.local>
2017-12-03 14:58:08 -0700

    CL done, untested

--- src/Clusterers/CompetitiveLearning.java
+++ src/Clusterers/CompetitiveLearning.java
@@ -1,15 +1,18 @@
 package Clusterers;
 
-import Data.Clustering;
-import Data.Dataset;
+import Data.*;
+
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Random;
 
 public class CompetitiveLearning implements IDataClusterer {
 
     private List<Neuron> inputs;
     private List<Neuron> outputs;
+    private Cluster[] clusters;
 
     private final double learningRate;
 
@@ -29,10 +32,64 @@ public class CompetitiveLearning implements IDataClusterer {
             initializeNetwork(dataset.getFeatureSize(), outputs.size());
         }
 
+        //randomly select cluster centers and create clusters
+        Random random = new Random();
+        for (int i = 0; i < outputs.size(); i++) {
+            int randomIndex = random.nextInt(dataset.size() - i);
+            double[] featuresCenter = dataset.get(randomIndex).features;
+            clusters[i] = new Cluster(dataset, i);
+            clusters[i].setClusterCenter(featuresCenter);
+            outputs.get(i).setFeatureOrCenter(clusters[i].getClusterCenter());
+
+            Collections.swap(dataset, randomIndex, dataset.size() - i);
+
+        }
+        //while not convergence
+        int epoch = 0;
+        boolean recluster = true;
+
+        while(recluster) {
+            int dataLeft = dataset.size() - outputs.size();
+
+            //for each data point, break up into features and assign one feature/input neuron
+            //then do the competitive learning stuff
+            for(Datum datum : dataset) {
+
+                recluster = false;
+                //calculate the distance between each output neuron and the features
+                //save the output that has the minimum distance
+                double minDist = datum.computeDistance(outputs.get(0).getFeatureOrCenter());
+                int clusterID = 0;
+
+                for(int i = 1; i <outputs.size();i++){
+                    double currentDist = datum.computeDistance(outputs.get(i).getFeatureOrCenter());
+
+                    if(currentDist < minDist){
+                        minDist = currentDist;
+                        clusterID = i;
+                    }
+                }
+                //move the winning cluster closer to the inputs
+                if(outputs.get(clusterID).getFeatureOrCenter() != updateCenter(outputs.get(clusterID))) {
+                    outputs.get(clusterID).setFeatureOrCenter(updateCenter(outputs.get(clusterID)));
+                    recluster = true;
+                }
+
+                //update cluster centers to be equal to the output neuron features
+                clusters[clusterID].setClusterCenter(outputs.get(clusterID).getFeatureOrCenter());
+
+
+            }
+            //evaluate clusters
+            System.out.println("Epoch: " + epoch + "\t\t");
+            clustering.evaluateClusters();
+            epoch++;
+        }
 
         return clustering;
     }
 
+    //create network
     private void initializeNetwork(int inputs, int outputs) {
         this.inputs = new ArrayList<>(inputs);
         this.outputs = new ArrayList<>(outputs);
@@ -45,4 +102,15 @@ public class CompetitiveLearning implements IDataClusterer {
             this.outputs.add(new Neuron(inputs));
         }
     }
+
+    //center update rule
+    private double[] updateCenter(Neuron center){
+        double[] newCenter = center.getFeatureOrCenter();
+        for(int c = 0; c < inputs.size(); c++){
+            newCenter[c] = learningRate*(inputs.get(c).getFeatureOrCenter()[0] - newCenter[c]);
+        }
+        return newCenter;
+
+    }
+
 }

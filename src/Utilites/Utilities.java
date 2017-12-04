376af83ef9f53b84d7dd28a51c39e7d02939d46d
Zach Connelly <zmconnelly@gmail.com>
2017-12-04 10:07:46 -0700

    Update utils

--- src/Utilites/Utilities.java
+++ src/Utilites/Utilities.java
@@ -1,5 +1,6 @@
 package Utilites;
 
+import java.io.*;
 import java.util.Random;
 
 public class Utilities {
@@ -14,7 +15,23 @@ public class Utilities {
         return randomDouble(upperBound + lowerBound) - lowerBound;
     }
 
-    public static double randomDouble(int bound) {
-        return random.nextDouble() * bound;
+    public static double randomDouble(int upperBound) {
+        return random.nextDouble() * upperBound;
+    }
+
+    public static void setFileOut(String filename) {
+        setFileOut(filename, ".txt");
+    }
+
+    public static void setFileOut(String filename, String extension) {
+        try {
+            System.setOut(new PrintStream(new BufferedOutputStream(new FileOutputStream(filename.concat(extension), false)), true));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void setConsoleOut() {
+        System.setOut(new PrintStream(new FileOutputStream(FileDescriptor.out)));
     }
 }

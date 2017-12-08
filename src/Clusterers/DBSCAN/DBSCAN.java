bd661a44c70ac152a5d3ac7f5cc9770780c3d0f4
Zach Connelly <zmconnelly@gmail.com>
2017-12-08 15:59:56 -0700

    Comments/cleanup DBSCAN

--- src/Clusterers/DBSCAN/DBSCAN.java
+++ src/Clusterers/DBSCAN/DBSCAN.java
@@ -6,35 +6,44 @@ import Utilites.Utilities;
 
 import java.util.stream.Collectors;
 
+/**
+ * An implementation of DBSCAN
+ * Class must be initialized with minPts and epsilon
+ * May then be used to cluster datasets using the Cluster(Dataset dataset) methods
+ *
+ * @author Zach Connelly
+ */
 public class DBSCAN implements IDataClusterer {
 
     private final double epsilon;
     private final int minPoints;
-    private int currentClusterLbl;
 
-    public DBSCAN(int minPoints, double maxDistance) {
+    public DBSCAN(int minPoints, double epsilon) {
         this.minPoints = minPoints;
-        this.epsilon = maxDistance;
-        this.currentClusterLbl = 1;
+        this.epsilon = epsilon;
     }
 
     @Override
     public Clustering cluster(Dataset dataset) {
         Clustering clustering = new Clustering();
-        currentClusterLbl = 1;
+        int currentClusterLbl = 1;
 
         // Mark all core points
         findCorePoints(dataset);
 
+        // For each example in the dataset
         for (Datum datum : dataset) {
+            // Skip for non core points
             if (!datum.isCore()) {
                 continue;
             }
+            // If not yet assigned a cluster
             if (datum.getCluster() == 0) {
                 datum.setCluster(currentClusterLbl);
                 currentClusterLbl++;
             }
 
+            // Assign the same cluster label to neighbors that haven't be assigned a label
             Dataset neighbors = getNeighbors(datum, dataset);
             for (Datum neighbor : neighbors) {
                 if (neighbor.getCluster() == 0) {
@@ -43,18 +52,22 @@ public class DBSCAN implements IDataClusterer {
             }
         }
 
+        // Sort the dataset according to cluster assignment
         dataset.sortByCluster();
+
+        // Loop over the dataset grouping points with the same labels into clusters
         Cluster cluster = new Cluster(dataset.get(0).getCluster());
         clustering.add(cluster);
-        for (Datum next : dataset) {
-            if (cluster.getClusterId() == next.getCluster()) {
-                cluster.add(next);
-            } else {
+        for (Datum point : dataset) {
+            if (cluster.getClusterId() == point.getCluster()) {     // Add to existing cluster
+                cluster.add(point);
+            } else {                                                // Form new cluster
                 clustering.add(cluster);
-                cluster = new Cluster(next.getCluster());
-                cluster.add(next);
+                cluster = new Cluster(point.getCluster());
+                cluster.add(point);
             }
         }
+
         return clustering;
     }
 
@@ -68,11 +81,11 @@ public class DBSCAN implements IDataClusterer {
     }
 
     /**
-     * Collect the list of data points within epsilon of the target point
+     * Computes a list of data points within epsilon of the seed point
      */
-    private Dataset getNeighbors(Datum sample, Dataset dataset) {
+    private Dataset getNeighbors(Datum seed, Dataset dataset) {
         return dataset.parallelStream()
-                .filter(neighbor -> !neighbor.equals(sample) && Utilities.computeDistance(sample.features, neighbor.features) < this.epsilon)
+                .filter(neighbor -> !neighbor.equals(seed) && Utilities.computeDistance(seed.features, neighbor.features) < this.epsilon)
                 .collect(Collectors.toCollection(Dataset::new));
     }
 
